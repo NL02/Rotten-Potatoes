806df386f41f8edb7d095828105115544868289d
nelsonliang <nl3514@g.rit.edu>
2020-01-02 19:12:51 -0500

    Users can delete reviews

--- app.js
+++ app.js
@@ -91,3 +91,12 @@ app.put('/reviews/:id', (req,res) => {
     })
 })
 
+//DELETE
+app.delete('/reviews/:id', function (req,res){
+    console.log("DELETE review")
+    Review.findByIdAndRemove(req.params.id).then((review) =>{
+        res.redirect('/')
+    }).catch((err) => {
+        console.log(err.message)
+    })
+})
